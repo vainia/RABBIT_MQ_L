1dcc53b92dac1fc4c14acf47a3ac2f3c66a58e8f
Ivan Napolskykh <Vanias2806@gmail.com>
2018-05-17 19:36:36 +0200

    Config correct & exec prop add

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ SET={
     'port':'5672',
     'server':'185.52.193.84',
     'name':'inapolskych',
-    'passwd':'mySuperSecretPassword129A'
-    'queue_current':'RMQ-L-W'
+    'passwd':'mySuperSecretPassword129A',
+    'queue_current':'RMQ-L-W',
     'queve_above':'RMQ-W-USER'
 }
